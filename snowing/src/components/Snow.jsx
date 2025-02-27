4e4b031c09e7154c928b81704c6265c96345e4f5
Barbora6 <smetanovabarbora@email.cz>
2025-02-27 13:41:52 +0100

    vymazání stylu

--- snowing/src/components/Snow.jsx
+++ snowing/src/components/Snow.jsx
@@ -3,10 +3,6 @@ import "./Snow.css";
 export const Snow = (props) => {
   return (
     <>
-      {/* <div className="snow-container">
-        <h1>Sněžení</h1>
-      </div>
-      <div className="snow-icon"></div> */}
       <p className="snowFlake" id={`item${props.id}`} style={props.style}>
         *
       </p>
