88236b915f23507091bcfc5d556f24750fd5d394
Barbora6 <smetanovabarbora@email.cz>
2025-02-27 13:32:41 +0100

    snowing

--- snowing/src/App.js
+++ snowing/src/App.js
@@ -1,10 +1,26 @@
-import { Snow } from "./components/Snow";
 import "./App.css";
+import { Snow } from "./components/Snow";
 
 export const App = () => {
+  let snowing = () => {
+    let animationDelay = "0s";
+    let fontSize = "100px";
+    let arr = Array.from("Snowflakes are awesome!!!");
+    return arr.map((el, i) => {
+      animationDelay = `${(Math.random() * 16).toFixed(2)}s`;
+      fontSize = `${Math.floor(Math.random() * 10) + 10}px`;
+      let style = {
+        animationDelay,
+        fontSize
+      };
+      return <Snow key={i} id={i} style={style} />;
+    });
+  };
+
   return (
     <div className="container">
-      <Snow />
+      <h1>sněží</h1>
+      {snowing()}
     </div>
   );
 };
--- snowing/src/components/Snow.jsx
+++ snowing/src/components/Snow.jsx
@@ -1,12 +1,15 @@
 import "./Snow.css";
 
-export const Snow = () => {
+export const Snow = (props) => {
   return (
     <>
-      <div className="snow-container">
+      {/* <div className="snow-container">
         <h1>Sněžení</h1>
       </div>
-      <div className="snow-icon"></div>
+      <div className="snow-icon"></div> */}
+      <p className="snowFlake" id={`item${props.id}`} style={props.style}>
+        *
+      </p>
     </>
   );
 };
